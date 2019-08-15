475071921cc71f66c96dc4fd4ac3fc3a519a1ff6
Orlan-Asuncion <48038688+Orlan-Asuncion@users.noreply.github.com>
2019-08-15 15:50:53 -0400

    update server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get("/", function(req, res) {
       }
     });
 });
-// use cheerio to scrape stories from TechCrunch and store them
+// use cheerio to scrape stories from npr.org and store them
 app.get("/scrape", function(req, res) {
   request("https://www.npr.org/sections/technology/", function(error, response, html) {
     // Load the html body from request into cheerio
@@ -97,22 +97,38 @@ app.get("/scrape", function(req, res) {
 });
 
 // route for retrieving all the saved articles
-app.get("/saved", function(req, res) {
-  db.Article.find({
-      saved: true
-    })
-    .then(function(dbArticle) {
-      // if successful, then render with the handlebars saved page
-      res.render("saved", {
-        articles: dbArticle
-      });
-    })
-    .catch(function(err) {
+ app.get("/saved", function(req, res) {
+   db.Article.find({
+       saved: true
+     })
+     .then(function(dbArticle) {
+       // if successful, then render with the handlebars saved page
+       res.render("saved", {
+         articles: dbArticle
+       });
+     })
+     .catch(function(err) {
       // If an error occurs, send the error back to the client
-      res.json(err);
+       res.json(err);
+     });
     });
-
-});
+// });
+// app.put("./saved/:id", function(req, res) {
+//      db.Article.findByIdAndUpdate(
+//          req.params.id, {
+//            $set: req.body
+//          }, {
+//            new: true
+//          })
+//        .then(function(dbArticle) {
+//          res.render("saved", {
+//            articles: dbArticle
+//          });
+//        })
+//        .catch(function(err) {
+//          res.json(err);
+//        });
+//    });
 
 // route for setting an article to saved
 app.put("/saved/:id", function(req, res) {
@@ -197,107 +213,7 @@ app.get("/notes/:id", function(req, res) {
 //       .children("a")
 //       .attr("href");
 
-//       console.log(result);
-//       //Create a new Article using the result object built from scraping
-//       db.Article.create(result)
-//       .then(function(dbArticle){
-//         console.log(dbArticle);
-//       })
-//       .catch(function(err){
-//         // if error occured, log it
-//        console.log(err);
-//       });
-//     });
-//     //send message to the client
-//     res.send("Scrape Complete");
-//   });
-// });
-// // Route for getting all Articles from the db
-// app.get("/articles", function(req, res) {
-//   // Grab every document in the Articles collection
-//   db.Article.find({})
-//     .then(function(dbArticle) {
-//       // If we were able to successfully find Articles, send them back to the client
-//       res.json(dbArticle);
-//     })
-//     .catch(function(err) {
-//       // If an error occurred, send it to the client
-//       res.json(err);
-//     });
-// });
-// // Route for grabbing a specific Article by id, populate it with it's note
-// app.get("/articles/:id", function(req, res) {
-//   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-//   db.Article.findOne({ _id: req.params.id })
-//     // ..and populate all of the notes associated with it
-//     .populate("note")
-//     .then(function(dbArticle) {
-//       // If we were able to successfully find an Article with the given id, send it back to the client
-//       res.json(dbArticle);
-//     })
-//     .catch(function(err) {
-//       // If an error occurred, send it to the client
-//       res.json(err);
-//     });
-// });
-// app.put("/saved/:id", function(req, res) {
-//   db.Article.findByIdAndUpdate(
-//       req.params.id, {
-//         $set: req.body
-//       }, {
-//         new: true
-//       })
-//     .then(function(dbArticle) {
-//       res.render("saved", {
-//         articles: dbArticle
-//       });
-//     })
-//     .catch(function(err) {
-//       res.json(err);
-//     });
-// });
 
-// // Route for saving/updating an Article's associated Note
-// app.post("/articles/:id", function(req, res) {
-//   // Create a new note and pass the req.body to the entry
-//   db.Note.create(req.body)
-//     .then(function(dbNote) {
-//       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-//       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-//       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-//       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-//     })
-//     .then(function(dbArticle) {
-//       // If we were able to successfully update an Article, send it back to the client
-//       res.json(dbArticle);
-//     })
-//     .catch(function(err) {
-//       // If an error occurred, send it to the client
-//       res.json(err);
-//     });
-// });
-// app.get("/comments/article/:id", function(req, res) {
-//   db.Article.findOne({"_id":req.params.id})
-//     .populate("comments")
-//     .exec (function (error, data) {
-//         if (error) {
-//             console.log(error);
-//         } else {
-//           res.json(data);
-//         }
-//     });        
-// });
-
-// app.get("/comments/:id", function(req, res) {
-
-//   db.Comment.findOneAndRemove({_id:req.params.id}, function (error, data) {
-//       if (error) {
-//           console.log(error);
-//       } else {
-//       }
-//       res.json(data);
-//   });
-// });
 // Start the server
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
